64be35167eb9e7423eb8f99a476698685d10ea60
上冬辰 <sun545949068@163.com>
2024-11-26 00:03:30 +0800

    修改

--- scripts/main.sh
+++ scripts/main.sh
@@ -3,8 +3,8 @@
 #git初始化，每次初始化不影响推送
 git init
 git add -A
-git commit -m 'dancysu'
-git branch -M main
+git commit -m 'deploy'
+git branch -M master
 
 # 如果你想要发布到 https://<USERNAME>.github.io
-git push -f git@github.com:dancysu/dancysu.github.io.git main
+git push -f git@github.com:sunwenqiangy/vitepress-blog.git master
--- scripts/pages.sh
+++ scripts/pages.sh
@@ -7,7 +7,7 @@ set -e
 rm -rf ./docs/.vitepress/dist/*
 
 # 生成静态文件 yarn docs:build 
-yarn build
+pnpm build
 
 # 进入生成的文件夹
 cd ./docs/.vitepress/dist
@@ -15,8 +15,8 @@ cd ./docs/.vitepress/dist
 #git初始化，每次初始化不影响推送
 git init
 git add -A
-git commit -m 'dancysu'
+git commit -m 'commit'
 git branch -M pages
 
 # 如果你想要发布到 https://<USERNAME>.github.io
-git push -f git@github.com:dancysu/dancysu.github.io.git pages
+git push -f git@github.com:sunwenqiangy/vitepress-blog.git pages
